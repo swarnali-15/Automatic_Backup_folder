c9e3bacf9f18a2dd33182c8a0c91563104eb3ed9
Swarnali Nandy <91903231+swarnali-15@users.noreply.github.com>
2024-11-15 21:42:28 +0530

    Add files via upload

---
+++ project_1_automatic_backup_folder.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+import datetime
+import schedule  
+import time
+
+source_dir= r"C:\Users\Swarnali\OneDrive\Pictures\Camera Roll"
+destination_dir= r"C:\Users\Swarnali\OneDrive\Pictures\backup"
+
+def copy_folder_to_directory(source,dest):
+    today=datetime.date.today()
+    dest_dir=os.path.join(dest,str(today))
+    try:
+        shutil.copytree(source,dest_dir)
+        print(f"Folder copied to:{dest_dir} ")
+    except FileExistsError:
+        print(f"Folder already exist in: {dest}")
+
+schedule.every().day.at("11:00").do (lambda: copy_folder_to_directory(source_dir,destination_dir))       
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)
+
+
+
+
+
